a4aedc30d4f21759609cf47305c85b67e9d63870
lam300695 <lam300695@gmail.com>
2020-02-27 09:18:41 +0700

    Servlet Time

---
+++ src/SeverTimeServlet.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+@javax.servlet.annotation.WebServlet(name = "SeverTimeServlet",urlPatterns = "/index")
+public class SeverTimeServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<h1>Hello Word</h1>");
+        Date today = new Date();
+        writer.println("<h1>" + today + "</h1>");
+
+        writer.println("</html>");
+    }
+}
